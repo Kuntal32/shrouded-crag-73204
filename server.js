0b9db3bbbfc8322144bc7abb2615d7a90919a044
kuntal <kuntal@nettrackers.net>
2020-10-25 13:57:29 +0530

    test2

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require("express");
 var app=express();
 
 app.get('/',function(req,res){
-res.sendFile(__dirname+'/index.html');
-
+//res.sendFile(__dirname+'/index.html');
+console.log("hellow world");
 });
 
 app.listen(3000,function(){
